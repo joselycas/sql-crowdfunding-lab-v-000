d49ef25224bcaf3d2aa5225f17c99e3c130ff9b6
Jos Castro <joselycas@gmail.com>
2018-08-09 18:20:32 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,12 +32,11 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, SUM(pledges.amount
- FROM projects
+"SELECT users.name, SUM(pledges.amount)
+ FROM users
  JOIN pledges
- ON pledges.project_id = projects.id
- GROUP BY projects.title
- HAVING projects.funding_goal <= SUM(pledges.amount)"
+ ON pledges.project_id = users.id
+ GROUP BY SUM(pledges.amount), users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
