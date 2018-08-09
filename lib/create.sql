003813d66f8090b837f5b7f8e0d267d56cad6dd0
Jos Castro <joselycas@gmail.com>
2018-08-09 16:51:11 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,2 +1,2 @@
 CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date INTEGER, end_date INTEGER)
-CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age TEXT)
+-- CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age TEXT)
