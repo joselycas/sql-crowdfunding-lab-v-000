1db7bb3d412e01e0a0168d573654c554c17a5869
Jos Castro <joselycas@gmail.com>
2018-08-09 18:25:59 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,7 +36,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
  FROM users
  JOIN pledges
  ON pledges.project_id = users.id
- GROUP BY users.name HAVING SUM(pledges.amount)"
+ GROUP BY pledges.amount"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
