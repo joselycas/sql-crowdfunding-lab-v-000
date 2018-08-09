3b7c9a2028000661e68bd1d9c058185c4b759d8c
Jos Castro <joselycas@gmail.com>
2018-08-09 18:12:42 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
  JOIN pledges
  ON pledges.project_id = projects.id
  GROUP BY projects.title
- HAVING projects.funding_goal <= SUM(ledges.amount)
+ HAVING projects.funding_goal <= SUM(Pledges.amount)
 "
 end
 
