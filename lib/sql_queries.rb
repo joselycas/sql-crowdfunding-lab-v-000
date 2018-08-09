9a13705698920e16855adfe599dfcd84fc360784
Jos Castro <joselycas@gmail.com>
2018-08-09 17:29:22 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT title
  FROM Projects
  JOIN Pledges
- ON project_id = id
+ ON project_id.pledges = id.projects
  ORDER BY title.projects"
 end
 
