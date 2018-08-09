0b919a52bd4b67882566ae9d16943118bae18b67
Jos Castro <joselycas@gmail.com>
2018-08-09 18:27:06 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,7 +36,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
  FROM users
  JOIN pledges
  ON pledges.project_id = users.id
- GROUP BY pledges.amount DESC"
+ ORDER BY pledges.amount DESC"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
