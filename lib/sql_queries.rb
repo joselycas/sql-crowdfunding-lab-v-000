bc54f9050a72a9c063d98b932afd3a88b8589412
Jos Castro <joselycas@gmail.com>
2018-08-09 18:56:28 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,7 +34,7 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT users.name, pledges.amount
  FROM users
- OUTER JOIN pledges
+ JOIN pledges
  ON pledges.project_id = users.id
  GROUP BY users.name
  ORDER BY pledges.amount, users.name"
